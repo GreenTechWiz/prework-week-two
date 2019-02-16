0e707f7454198a7f6bcbf2457f77db6c66820f32
GreenTechWiz <aggreen0405@gmail.com>
2019-02-15 22:48:28 -0600

    Classes as Constructors Prework

--- car.rb
+++ car.rb
@@ -1,21 +1,33 @@
 class Car
 
-  def mileage
-    return 12
+  attr_accessor :mileage, :color, :miles
+
+  def initialize(mileage, color)
+    @mileage = mileage
+    @color = color
   end
 
-  def color
-    return "Green, cause it's better then red"
+  def drive(miles)
+    puts "*Drives #{miles} miles*"
+    miles + mileage
   end
+  # def mileage
+  #   return 12
+  # end
+
+  # def color
+  #   return "Green, cause it's better then red"
+  # end
+
+
 
   def honk_horn
     puts "*Honk!*"
   end
 end
 
-wheels = Car.new
-
-puts wheels.mileage
+wheels = Car.new(12, "Green, cause it's better then red")
+puts wheels.drive(5)
 puts wheels.color
 puts wheels.honk_horn
 
